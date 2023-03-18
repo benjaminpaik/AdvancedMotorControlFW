f02ac54a5656df26b4a17f8ee45bf8304b5c4022
bpaik <benjaminpaik@gmail.com>
2023-03-17 23:29:47 -0700

    Migrated CubeMX version.

--- Core/Src/stm32g4xx_hal_timebase_tim.c
+++ Core/Src/stm32g4xx_hal_timebase_tim.c
@@ -45,7 +45,7 @@ HAL_StatusTypeDef HAL_InitTick(uint32_t TickPriority)
   uint32_t              uwTimclock = 0;
   uint32_t              uwPrescalerValue = 0;
   uint32_t              pFLatency;
-  HAL_StatusTypeDef     status = HAL_OK;
+  HAL_StatusTypeDef     status;
 
   /* Enable TIM17 clock */
   __HAL_RCC_TIM17_CLK_ENABLE();
@@ -55,6 +55,7 @@ HAL_StatusTypeDef HAL_InitTick(uint32_t TickPriority)
 
   /* Compute TIM17 clock */
   uwTimclock = HAL_RCC_GetPCLK2Freq();
+
   /* Compute the prescaler value to have TIM17 counter clock equal to 1MHz */
   uwPrescalerValue = (uint32_t) ((uwTimclock / 1000000U) - 1U);
 
@@ -62,6 +63,7 @@ HAL_StatusTypeDef HAL_InitTick(uint32_t TickPriority)
   htim17.Instance = TIM17;
 
   /* Initialize TIMx peripheral as follow:
+
   + Period = [(TIM17CLK/1000) - 1]. to have a (1/1000) s time base.
   + Prescaler = (uwTimclock/1000000 - 1) to have a 1MHz counter clock.
   + ClockDivision = 0
